a10555d8ced37380cc075b41ea99efc97f90c1da
Kory <KoryThacher@gmail.com>
2018-03-20 11:59:12 -1000

    initial commit
    initial commit

---
+++ AddNode.ps1
@@ -0,0 +1,21 @@
+﻿$pathToConfig = "$PSScriptRoot\DistributedCacheService.exe.config" #put your path here
+ 
+#force the config into an XML
+$xml = [xml](get-content $pathToConfig)
+ 
+#find the node to insert after
+$foundNode = $xml.configuration.configsections
+ 
+#build new node by hand and force it to be an XML object
+$newNode = [xml]@"
+<appSettings>
+    <add key="backgroundGC" value="true"/>
+</appSettings>
+"@
+ 
+#add new node AFTER the configsections node
+$newNode = $xml.ImportNode($newNode.appSettings,$true) 
+$xml.configuration.InsertAfter($newNode,$foundNode) |out-null
+ 
+#save file
+$xml.Save("$pathToConfig.CHANGED.XML")
---
+++ AddNodeShort.ps1
@@ -0,0 +1,17 @@
+﻿$pathToConfig = "$PSScriptRoot\DistributedCacheService.exe.config" #put your path here
+
+#force the config into an XML
+$xml = [xml](get-content $pathToConfig)
+
+#build new node by hand and force it to be an XML object
+[xml]$newNode = @"
+<appSettings>
+    <add key="backgroundGC" value="true"/>
+</appSettings>
+"@
+
+#add new node AFTER the configsections node
+$xml.configuration.InsertAfter($xml.ImportNode($newNode.appSettings, $true), $xml.configuration.configsections) | out-null
+
+#save file
+$xml.Save($pathToConfig)
